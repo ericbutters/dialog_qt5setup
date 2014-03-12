d7181a006cb8e07d4aea52b5c5dd3a856f8e7899
Daniel <daniel@eric-X220tp.(none)>
2014-03-13 00:32:14 +0100

    Add radiolist to select version

--- dialog_qtsetup.sh
+++ dialog_qtsetup.sh
@@ -17,11 +17,19 @@ esac
 rm -f /tmp/input.$$
 #sleep 2
 
+version=$(dialog --title "Qt5 Version" --backtitle "$BT" \
+             --radiolist "choose version" 15 50 8 \
+       Qt-5.0.2 "" off \
+       Qt-5.1.0 "" on \
+       Qt-5.2.0 "" off \
+       2>&1 >/dev/tty )
+
+QT_VERSION=$(echo $version | cut -d- -f2)
+QT_MAJOR=$(echo $QT_VERSION | cut -d. -f1,2)
 
 cd /tmp
-QT_VERSION="5.0.2"
 QT_ARCHIVE=qt-everywhere-opensource-src-$QT_VERSION.tar.gz
-URL="http://ftp.fau.de/qtproject/archive/qt/5.0/$QT_VERSION/single/$QT_ARCHIVE"
+URL="http://ftp.fau.de/qtproject/archive/qt/$QT_MAJOR/$QT_VERSION/single/$QT_ARCHIVE"
 wget "$URL" 2>&1 | \
  stdbuf -o0 awk '/[.] +[0-9][0-9]?[0-9]?%/ { print substr($0,63,3) }' | \
  dialog --title "WGET" --backtitle "$BT" \
